8ca48ee46e69b40ca4cfdeaa8db141dbfa113858
WaluigiBSOD <69278068+WaluigiBSOD@users.noreply.github.com>
2024-04-14 22:35:18 +0200

    Removed redundant function call in io.js
    I bet this is a debug remnant of some sort from some early work in progress version of dm64-password-decoder I forgot to remove in all these years (I no longer have any of them so I can't check to confirm).

--- js/info.js
+++ js/info.js
@@ -20,8 +20,8 @@
 
 const Title = "Mario Kart: Double Dash!! Password Decoder";
 
-const Version = "1.2.1";
-const VersionDate = "28 January 2024";
+const Version = "1.2.2";
+const VersionDate = "14 April 2024";
 
 // Functions
 
